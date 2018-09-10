9d2841752de2bb5f19844d3d76bc20dc2fb58fd4
Stephen Szpak <stephenszpak@Stephens-MacBook-Pro.local>
2018-09-10 12:43:08 -0500

    create a UI to create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :redirect_if_not_signed_in, only: [:new]
 
   def show
    @post = Post.find(params[:id])
@@ -21,13 +22,6 @@ class PostsController < ApplicationController
     format.js { render partial: 'posts/posts_pagination_page' }
   end
 
-  private
-
-  def posts_for_branch(branch)
-    @categories = Category.where(branch: branch)
-    @posts = get_posts.paginate(page: params[:page])
-  end
-
   def get_posts
     PostsForBranchService.new({
       search: params[:search],
@@ -35,5 +29,33 @@ class PostsController < ApplicationController
       branch: params[:action]
     }).call
   end
+
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def posts_for_branch(branch)
+    @categories = Category.where(branch: branch)
+    @posts = get_posts.paginate(page: params[:page])
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id)
+                         .merge(user_id: current_user.id)
+  end
+
 
 end
